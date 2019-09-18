b8114dedfa4716060be96618198039f19f1dcf48
1298743328szyasy <1298743328@qq.com>
2019-09-18 19:07:33 +0800

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] str = {3,5,8,7};
+            int first = str[0];
+            for (int i = 1; i < str.Length; i++)
+            {
+                str[i - 1] = str[i];
+            }
+            str[str.Length-1] = first;
+            foreach (int a in str)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
